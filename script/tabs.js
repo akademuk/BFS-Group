d404e092cb1156f01f284947766a6a47d59197ee
akademuk <akademuk24@gmail.com>
2023-01-11 11:47:38 +0200

    sss

--- script/tabs.js
+++ script/tabs.js
@@ -1,7 +1,7 @@
 $(".tab").click(function() {
 	$(".tab").removeClass("active").eq($(this).index()).addClass("active");
 	$(".tab_item").hide().eq($(this).index()).fadeIn()
-});
+}).eq(0).addClass("active");
 
 $(".tabItemSection").click(function() {
     $(".tabItemSection").toggleClass("active");
